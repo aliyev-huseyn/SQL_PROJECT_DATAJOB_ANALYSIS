e6bdbcf3a57d7191d814ec5d3cc030d5ba5d1078
Aliyev Huseyn <aliyev.huseyn.work@gmail.com>
2025-12-08 15:55:36 +0400

    top_payingjobs

---
+++ project_sql/top_paying_jobs1.sql
@@ -0,0 +1,22 @@
+/*
+Question: What are the top-paying data analyst jobs?
+- Identify the top 10 highest-paying Data Analyst roles that are available remotely.
+- Focuses on job postings with specified salaries (remove nulls).
+- Why? Highlight the top-paying opportunities for Data Analysts, offering insights into employment
+*/
+
+SELECT 
+    job_id, 
+    c.name as company_name,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date
+FROM job_postings_fact
+LEFT JOIN company_dim c ON job_postings_fact.company_id = c.company_id
+    WHERE job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL
+    AND job_work_from_home IS TRUE
+ORDER BY salary_year_avg DESC
+LIMIT 10;
